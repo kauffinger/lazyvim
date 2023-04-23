028bb704de29636b7c30e00d598ba001409cc514
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-04-23 18:25:56 +0200

    Xdebug Running 🎉

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -8,7 +8,28 @@ return {
     automatic_setup = true,
     -- You can provide additional configuration to the handlers,
     -- see mason-nvim-dap README for more information
-    handlers = {},
+    handlers = {
+      function(config)
+        -- all sources with no handler get passed here
+
+        -- Keep original functionality
+        require('mason-nvim-dap').default_setup(config)
+      end,
+      php = function(config)
+        config.configurations = {
+          {
+            type = 'php',
+            request = 'launch',
+            name = 'Listen for Xdebug',
+            port = 9003,
+            pathMappings = {
+              ['/var/www/html'] = "${workspaceFolder}",
+            },
+          }
+        }
+        require('mason-nvim-dap').default_setup(config) -- don't forget this!
+      end,
+    },
     -- You'll need to check that you have the required things installed
     -- online, please don't ask me how to install them :)
     ensure_installed = {
