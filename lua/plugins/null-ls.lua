be5064e3b662d9e171044c76184199dacfa7cd39
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-05-12 16:51:00 +0200

    Update nvim, cleanup, and increase fmt timeout

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,29 +1,38 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  opts = function()
-    local nls = require("null-ls")
-    return {
-      sources = {
-        nls.builtins.formatting.black,
-        nls.builtins.formatting.blade_formatter,
-        nls.builtins.diagnostics.eslint_d.with({
-          condition = function(utils)
-            return utils.root_has_file({ ".eslintrc.json" })
-          end,
-        }),
-        nls.builtins.diagnostics.trail_space.with({ disabled_filetypes = { "NvimTree" } }),
-        nls.builtins.formatting.eslint_d.with({
-          condition = function(utils)
-            return utils.root_has_file({ ".eslintrc.json" })
-          end,
-        }),
-        nls.builtins.formatting.prettierd,
-        nls.builtins.formatting.rustywind.with({
-          extra_filetypes = { "blade" },
-        }),
-        nls.builtins.formatting.pint,
-        nls.builtins.diagnostics.hadolint,
-      },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          nls.builtins.formatting.black,
+          nls.builtins.formatting.blade_formatter,
+          nls.builtins.diagnostics.eslint_d.with({
+            condition = function(utils)
+              return utils.root_has_file({ ".eslintrc.json" })
+            end,
+          }),
+          nls.builtins.diagnostics.trail_space.with({ disabled_filetypes = { "NvimTree" } }),
+          nls.builtins.formatting.eslint_d.with({
+            condition = function(utils)
+              return utils.root_has_file({ ".eslintrc.json" })
+            end,
+          }),
+          nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.rustywind.with({
+            extra_filetypes = { "blade" },
+          }),
+          nls.builtins.formatting.pint,
+          nls.builtins.diagnostics.hadolint,
+        },
+      }
+    end,
+  },
+  {
+    -- add longer timeout, since formatting blade files gets a little slow
+    "neovim/nvim-lspconfig",
+    opts = {
+      format = { timeout_ms = 2000 }
     }
-  end,
+  }
 }
