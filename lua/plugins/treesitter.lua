37b09062984b0b81b7590f20993c09b140f457f3
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-10-01 19:32:59 +0200

    feat: Add some more treesitter languages

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+-- add more treesitter parsers
+return
+{
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "bash",
+      "help",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
