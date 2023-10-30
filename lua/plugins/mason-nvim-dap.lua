33c30a8cb7273874c9124d8928a292f345d8dc4f
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-10-30 12:22:46 +0100

    feat: Enable usage with ordinary php installs

--- lua/plugins/mason-nvim-dap.lua
+++ lua/plugins/mason-nvim-dap.lua
@@ -13,30 +13,31 @@ return {
         -- all sources with no handler get passed here
 
         -- Keep original functionality
-        require('mason-nvim-dap').default_setup(config)
+        require("mason-nvim-dap").default_setup(config)
       end,
       php = function(config)
         config.configurations = {
           {
-            type = 'php',
-            request = 'launch',
-            name = 'Listen for Xdebug',
+            type = "php",
+            request = "launch",
+            name = "Listen for Xdebug",
             port = 9003,
             pathMappings = {
-              ['/var/www/html'] = "${workspaceFolder}",
+              ["/var/www/html"] = "${workspaceFolder}",
+              ["${workspaceFolder}"] = "${workspaceFolder}",
             },
-          }
+          },
         }
-        require('mason-nvim-dap').default_setup(config) -- don't forget this!
+        require("mason-nvim-dap").default_setup(config) -- don't forget this!
       end,
     },
     -- You'll need to check that you have the required things installed
     -- online, please don't ask me how to install them :)
     ensure_installed = {
       -- Update this to ensure that you have the debuggers for the langs you want
-      'php',
-      'bash',
-      'python',
+      "php",
+      "bash",
+      "python",
     },
   },
 }
