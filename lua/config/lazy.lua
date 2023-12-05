ca255b796bc9ea511ea05b6fc99d6876c658853b
Konstantin Auffinger <konstantinauffinger@gmail.com>
2023-12-05 22:19:30 +0100

    feat: add go extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
