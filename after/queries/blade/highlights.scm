3d98fd0b3c4b0145addc85b0914793551317ff2b
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-01-07 17:25:59 +0100

    feat: Improve highlighting a little (i have no idea what i am doing)

--- after/queries/blade/highlights.scm
+++ after/queries/blade/highlights.scm
@@ -2,3 +2,7 @@
 (directive_start) @tag
 (directive_end) @tag
 (comment) @comment
+((parameter) @include (#set! "priority" 110))
+((php_only) @include (#set! "priority" 110))
+((bracket_start) @function (#set! "priority" 120))
+((bracket_end) @function (#set! "priority" 120))
