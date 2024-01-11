8b8db53a3d9dfa88f319e8f2ed10fc9243182494
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-01-11 19:53:29 +0100

    feat: More stealing for blade stuff (this time calebdw/dotfiles)

--- after/queries/html/injections.scm
+++ after/queries/html/injections.scm
@@ -7,3 +7,29 @@
   (quoted_attribute_value
     (attribute_value) @injection.content)
   (#set! injection.language "javascript"))
+
+; Blade escaped JS attributes
+; <x-foo ::bar="baz" />
+(element
+  (_
+    (tag_name) @_tag
+      (#lua-match? @_tag "^x%-%l")
+  (attribute
+    (attribute_name) @_attr
+      (#lua-match? @_attr "^::%l")
+    (quoted_attribute_value
+      (attribute_value) @injection.content)
+    (#set! injection.language "javascript"))))
+
+; Blade PHP attributes
+; <x-foo :bar="$baz" />
+(element
+  (_
+    (tag_name) @_tag
+      (#lua-match? @_tag "^x%-%l")
+    (attribute
+      (attribute_name) @_attr
+        (#lua-match? @_attr "^:%l")
+      (quoted_attribute_value
+        (attribute_value) @injection.content)
+      (#set! injection.language "php_only"))))
--- queries/blade/injections.scm
+++ queries/blade/injections.scm
@@ -3,14 +3,13 @@
     (#set! injection.combined)
     (#set! injection.language php))
 
-; could be bash or zsh
-; or whatever tree-sitter grammar you have.
 ((text) @injection.content
     (#has-ancestor? @injection.content "envoy")
     (#set! injection.combined)
     (#set! injection.language bash))
 
 ((php_only) @injection.content
-   (#set! injection.language php_only))
+    (#set! injection.combined)
+    (#set! injection.language php_only))
 ((parameter) @injection.content
-   (#set! injection.language php_only))
+    (#set! injection.language php_only))
