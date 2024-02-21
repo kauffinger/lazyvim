5b97c5ce15a55e36f40211cdab1792303ae0d656
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-02-21 22:06:18 +0100

    fix: blade highlighting finally works, switching to ensure_installed all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,39 +18,32 @@ return {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
   opts = {
-    ensure_installed = {
-      "bash",
-      "blade",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "php",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
-      "vim",
-      "yaml",
-    },
+    ensure_installed = "all",
     auto_install = true,
     highlight = {
       enable = true,
     },
+    -- Needed because treesitter highlight turns off autoindent for php files
+    indent = {
+      enable = true,
+    },
   },
   config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
+    ---@class ParserInfo[]
     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     parser_config.blade = {
       install_info = {
         url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
+        files = {
+          "src/parser.c",
+          -- 'src/scanner.cc',
+        },
+        generate_requires_npm = true,
+        requires_generate_from_grammar = true,
       },
       filetype = "blade",
     }
+
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
--- queries/blade/highlights.scm
+++ queries/blade/highlights.scm
@@ -1,15 +1,4 @@
-([
-  (directive)
-  (directive_start)
-  (directive_end)
-] @keyword (#set! "priority" 110))
-
-([
-  (bracket_start)
-  (bracket_end)
-] @punctuation.special (#set! "priority" 110))
-
+(directive) @tag
+(directive_start) @tag
+(directive_end) @tag
 (comment) @comment @spell
-
-((parameter) @include (#set! "priority" 120))
-((php_only) @include (#set! "priority" 120))
