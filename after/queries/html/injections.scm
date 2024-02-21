615b15dcdb77599db24fcf8b2cecad5112090a7d
Konstantin Auffinger <konstantinauffinger@gmail.com>
2024-02-21 22:09:03 +0100

    feat: steal html injection updates from calebdw/dotfiles

--- after/queries/html/injections.scm
+++ after/queries/html/injections.scm
@@ -4,6 +4,7 @@
 (attribute
   (attribute_name) @_attr
     (#lua-match? @_attr "^x%-%l")
+    (#not-any-of? @_attr "x-teleport" "x-ref" "x-transition")
   (quoted_attribute_value
     (attribute_value) @injection.content)
   (#set! injection.language "javascript"))
