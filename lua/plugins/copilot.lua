768ae98e3c19fe8542e7d9c2aa3b848a977be9dd
ht3aa <ht3aa@gmail.com>
2024-07-30 20:43:10 +0300

    adding copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,45 +1,13 @@
 return {
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-
-    keys = {
-      { "<c-s>", "<CR>", ft = "copilot-chat", desc = "Submit Prompt", remap = true },
-      { "<leader>p", "", desc = "+ai", mode = { "n", "v" } },
-      {
-        "<leader>pa",
-        function()
-          return require("CopilotChat").toggle()
-        end,
-        desc = "Toggle (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>px",
-        function()
-          return require("CopilotChat").reset()
-        end,
-        desc = "Clear (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      {
-        "<leader>pq",
-        function()
-          local input = vim.fn.input("Quick Chat: ")
-          if input ~= "" then
-            require("CopilotChat").ask(input)
-          end
-        end,
-        desc = "Quick Chat (CopilotChat)",
-        mode = { "n", "v" },
-      },
-      -- Show help actions with telescope
-      { "<leader>pd", M.pick("help"), desc = "Diagnostic Help (CopilotChat)", mode = { "n", "v" } },
-      -- Show prompts actions with telescope
-      { "<leader>pp", M.pick("prompt"), desc = "Prompt Actions (CopilotChat)", mode = { "n", "v" } },
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  opts = {
+    suggestion = { enabled = true },
+    panel = { enabled = true },
+    filetypes = {
+      markdown = true,
+      help = true,
     },
-  },
-  { "zbirenbaum/copilot-cmp", enabled = true },
-  {
-    "zbirenbaum/copilot.lua",
   },
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,27 +1,27 @@
 return {
   "nvim-cmp",
   dependencies = {
-    -- codeium
     {
-      "Exafunction/codeium.nvim",
-      cmd = "Codeium",
-      build = ":Codeium Auth",
+      "zbirenbaum/copilot-cmp",
+      dependencies = "copilot.lua",
       opts = {},
+      config = function(_, opts)
+        local copilot_cmp = require("copilot_cmp")
+        copilot_cmp.setup(opts)
+        -- attach cmp source whenever copilot attaches
+        -- fixes lazy-loading issues with the copilot cmp source
+        LazyVim.lsp.on_attach(function(client)
+          copilot_cmp._on_insert_enter({})
+        end, "copilot")
+      end,
     },
   },
+  ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     table.insert(opts.sources, 1, {
-      name = "codeium",
+      name = "copilot",
       group_index = 1,
       priority = 100,
-    })
-  end,
-
-  setup = function(_, opts)
-    require("cmp").setup({
-      completion = {
-        keyword_length = 1,
-      },
     })
   end,
 }
