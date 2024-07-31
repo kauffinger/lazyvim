28be5633384ab035df379a0b76526b0c8d0d64e0
Hasan Tahseen <hasan.tahseen@ta3leem.dev>
2024-07-31 10:03:37 +0300

    change docs prompt in copilot chat

--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -1,11 +1,11 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
-  -- opts = function(_, opts)
-  --   -- Ensure the prompts table exists
-  --   opts.prompts = opts.prompts or {}
-  --   -- Override the Docs prompt
-  --   opts.prompts.Docs = {
-  --     prompt = "/COPILOT_GENERATE Please add documentation comment for the selection without lines number.",
-  --   }
-  -- end,
+  opts = function(_, opts)
+    -- Ensure the prompts table exists
+    opts.prompts = opts.prompts or {}
+    -- Override the Docs prompt
+    opts.prompts.Docs = {
+      prompt = "/COPILOT_GENERATE Please add documentation comment for the selection with all the code inside it.",
+    }
+  end,
 }
