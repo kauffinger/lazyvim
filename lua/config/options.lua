e9d044baff984d4a9a1196eec6a6f46b0aa24b70
ht3aa <ht3aa@gmail.com>
2024-08-02 19:30:28 +0300

    comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- so copilot doesn't map tab
 vim.g.copilot_no_tab_map = true
-vim.g.maplocalleader = ","
+
+-- MagicDuck/grug-far.nvim leader key
+vim.g.maplocalleader = " "
